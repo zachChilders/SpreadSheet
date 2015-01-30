ce486ef16a504a90ff5d9b04c937b08913b134e8
zachChilders <zwc101@gmail.com>
2015-01-29 16:08:14 -0800

    Basic GUI

---
+++ TinySpreadsheet/TinySpreadsheet/Cell.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TinySpreadsheet
+{
+    /// <summary>
+    /// Interaction logic for Cell.xaml
+    /// </summary>
+    public partial class Cell : UserControl
+    {
+        public Cell()
+        {
+            InitializeComponent();
+            BorderBrush = new SolidColorBrush(Colors.Black);
+            BorderThickness = new Thickness(0.25);
+            CellText.MouseDoubleClick += Cell_MouseDoubleClick;
+        }
+
+        void Cell_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            TextBox t = sender as TextBox;
+            t.IsReadOnly = !t.IsReadOnly;
+        }
+    }
+}
---
+++ TinySpreadsheet/TinySpreadsheet/Column.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TinySpreadsheet
+{
+    /// <summary>
+    /// Interaction logic for Column.xaml
+    /// </summary>
+    public partial class Column : UserControl
+    {
+        public Column()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 30; i++)
+            {
+                Cell c = new Cell();
+
+                CellColumn.Children.Add(c);
+            }
+        }
+    }
+}
