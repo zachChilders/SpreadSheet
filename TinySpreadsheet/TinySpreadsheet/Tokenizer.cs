7bbd6303615a1f3855254bf88fca5d4c95fc77f2
tylorhl <tylorlake@gmail.com>
2015-02-14 16:58:02 -0800

    Upgraded to Framework 4.0 for Tuple class. Started Tokenizer skeleton.

---
+++ TinySpreadsheet/TinySpreadsheet/Tokenizer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TinySpreadsheet
+{
+    public static class Tokenizer
+    {
+        public enum TokenType { CELL, OP, NUM, BANANA };
+        public static Queue<Tuple<String, Tokenizer.TokenType>> Tokenize(String formula)
+        {
+
+        }
+
+    }
+}
